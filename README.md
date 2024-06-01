8f2ce7a34324151794caf1629ec9edbd032ac980
Stephen Nolan <stepolan@users.noreply.github.com>
2024-05-31 20:16:31 -0500

    Update script and documentation for improved error handling and logging
    - Remove unused `shutil` import
    - Enhance `rsync` commands to include verbose output and error logging
    - Refactor user prompts for better UX
    - Update `.gitignore` to exclude log files
    - Update README with logging details and setup instructions

--- README.md
+++ README.md
@@ -2,35 +2,47 @@
 
 ARM2Intel Legacy Transfer is a tool that copies applications, preferences, documents, mail data, calendar data, and other non-hidden directories from a newer Mac with an Apple-based processor (ARM architecture) to an older Intel Mac with an older OS version. It ensures compatibility by excluding applications that are not universal binaries (compatible with both Intel and Apple Silicon architectures).
 
+This script was created because the Apple migration assistant does not let you migrate settings from a new Operating System to an older one. This is probably because things break when you do. Expect things to break, use at your own risk, and only use this on a fresh install of OSX on a wiped drive.
+
 ## Usage
 
+### Cloning the repo and creating an environment.
+
 1. Clone the repository.
+
 2. Create and activate a conda environment:
-   ```bash
+   ```
    conda create --name arm2intel_legacy_transfer python=3.9
    conda activate arm2intel_legacy_transfer
    ```
 3. Open VSCode and navigate to the project directory.
+
+### The next steps are recommended to avoid multiple prompts for a password.
+
 4. Generate SSH keys (if not already done):
-   ```bash
+   ```
    ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
    ```
    Press Enter to accept the default file location and enter a passphrase if you want (or leave it empty for no passphrase).
+
 5. Copy the public key to your older Mac:
-   ```bash
+   ```
    ssh-copy-id username@remote_ip
    ```
    Replace `username` and `remote_ip` with your actual username and the IP address of the older Mac. This will prompt you for the password one last time.
+
+### Running the script.
+
 6. Run the script:
-   ```bash
+   ```
    python3 arm2intel_legacy_transfer.py
    ```
+
 7. Enter the IP address and username of the older Mac when prompted.
-8. Select the transfer method (rsync or scp). 
-   - rsync: Recommended for faster, incremental transfers, and can resume from interruptions.
-   - scp: Simple, secure copy, but restarts the transfer from the beginning if interrupted.
-9. Respond to the prompts to decide whether to copy applications, Xcode (if present), the entire Library directory, preferences, documents, mail data, calendar data, and other non-hidden directories in the user home folder.
-10. The script will then proceed to copy the selected files and directories.
+
+8. Respond to the prompts to decide whether to copy applications, Xcode (if present), the entire Library directory, preferences, documents, mail data, calendar data, and other non-hidden directories in the user home folder.
+
+9. The script will then proceed to copy the selected files and directories. If there are any errors, they will be logged in `rsync_errors.log`. You can check this file to diagnose any issues.
 
 ## Disclaimer
 
--- arm2intel_legacy_transfer.py
+++ arm2intel_legacy_transfer.py
@@ -9,9 +9,12 @@ logging.basicConfig(filename='copy_mac_apps.log', level=logging.INFO, format='%(
 GREEN = '\033[92m'
 RED = '\033[91m'
 YELLOW = '\033[93m'
+MAGENTA = '\033[95m'
+DARK_MAGENTA = '\033[35m'
 BOLD = '\033[1m'
 ENDC = '\033[0m'
 
+# Function to check if the application is a universal binary
 def check_universal_binary(app_path):
     executable_path = os.path.join(app_path, "Contents", "MacOS")
     if not os.path.exists(executable_path):
@@ -30,6 +33,7 @@ def check_universal_binary(app_path):
     else:
         return False, output
 
+# Function to check the applications folder
 def check_applications_folder():
     applications_folder = "/Applications"
     app_files = os.listdir(applications_folder)
@@ -43,6 +47,7 @@ def check_applications_folder():
     
     return results
 
+# Functions to save results to files
 def save_results_to_file(results):
     with open("universal_binaries_report.txt", "w") as f:
         for app, (is_universal, details) in results.items():
@@ -62,7 +67,8 @@ def save_universal_apps_to_file(results):
             if is_universal:
                 f.write(f"{app}\n")
 
-def copy_all_except_non_intel_apps(remote_ip, username, copy_xcode, transfer_method):
+# Function to copy all applications except non-intel apps
+def copy_all_except_non_intel_apps(remote_ip, username, copy_xcode):
     with open("non_intel_apps.txt", "r") as f:
         non_intel_apps = [line.strip() for line in f]
     
@@ -77,24 +83,22 @@ def copy_all_except_non_intel_apps(remote_ip, username, copy_xcode, transfer_met
             if app == "Xcode.app":
                 if copy_xcode:
                     destination = f"{username}@{remote_ip}:/Applications/"
-                    if transfer_method == 'rsync':
-                        rsync_command = ["rsync", "-avvh", app_path, destination]
-                        subprocess.run(rsync_command)
+                    rsync_command = ["sudo", "rsync", "-avvh", "--rsync-path='sudo rsync'", app_path, destination, "2> rsync_errors.log"]
+                    result = subprocess.run(" ".join(rsync_command), shell=True)
+                    if result.returncode == 0:
+                        logging.info(f"Copied Xcode to {destination} successfully")
                     else:
-                        scp_command = ["scp", "-vr", app_path, destination]
-                        subprocess.run(scp_command)
-                    logging.info(f"Copied Xcode to {destination}")
+                        logging.error(f"Failed to copy Xcode to {destination}. Check rsync_errors.log for details.")
                 else:
                     not_copied_apps.append(app)
             elif app not in non_intel_apps:
                 destination = f"{username}@{remote_ip}:/Applications/"
-                if transfer_method == 'rsync':
-                    rsync_command = ["rsync", "-avh", app_path, destination]
-                    subprocess.run(rsync_command)
+                rsync_command = ["sudo", "rsync", "-avvh", "--rsync-path='sudo rsync'", app_path, destination, "2> rsync_errors.log"]
+                result = subprocess.run(" ".join(rsync_command), shell=True)
+                if result.returncode == 0:
+                    logging.info(f"Copied {app} to {destination} successfully")
                 else:
-                    scp_command = ["scp", "-vr", app_path, destination]
-                    subprocess.run(scp_command)
-                logging.info(f"Copied {app} to {destination}")
+                    logging.error(f"Failed to copy {app} to {destination}. Check rsync_errors.log for details.")
             else:
                 not_copied_apps.append(app)
     
@@ -102,106 +106,197 @@ def copy_all_except_non_intel_apps(remote_ip, username, copy_xcode, transfer_met
         for app in not_copied_apps:
             f.write(f"{app}\n")
     
-    if transfer_method == 'rsync':
-        rsync_command = ["rsync", "-avvh", "apps_to_manually_install.txt", f"{username}@{remote_ip}:/Applications/"]
-        subprocess.run(rsync_command)
+    rsync_command = ["sudo", "rsync", "-avvh", "--rsync-path='sudo rsync'", "apps_to_manually_install.txt", f"{username}@{remote_ip}:/Applications/", "2> rsync_errors.log"]
+    result = subprocess.run(" ".join(rsync_command), shell=True)
+    
+    if result.returncode == 0:
+        logging.info(f"Copied apps_to_manually_install.txt to {username}@{remote_ip}:/Applications/ successfully")
     else:
-        scp_command = ["scp", "-v", "apps_to_manually_install.txt", f"{username}@{remote_ip}:/Applications/"]
-        subprocess.run(scp_command)
-    logging.info(f"Copied apps_to_manually_install.txt to {username}@{remote_ip}:/Applications/")
+        logging.error(f"Failed to copy apps_to_manually_install.txt to {username}@{remote_ip}:/Applications/. Check rsync_errors.log for details.")
 
-def copy_additional_folder(remote_ip, username, folder, transfer_method):
-    destination = f"{username}@{remote_ip}:{folder.replace('~', '')}"
-    if transfer_method == 'rsync':
-        rsync_command = ["rsync", "-avvh", os.path.expanduser(folder), destination]
-        subprocess.run(rsync_command)
+# Function to copy additional folders
+def copy_additional_folder(remote_ip, username, folder):
+    source = os.path.expanduser(folder)
+    destination = f"{username}@{remote_ip}:~/{os.path.basename(folder)}"
+    
+    rsync_command = [
+        "sudo",
+        "rsync",
+        "-avvh",
+        "--rsync-path='sudo rsync'",
+        source,
+        destination,
+        "2> rsync_errors.log"
+    ]
+    result = subprocess.run(" ".join(rsync_command), shell=True)
+    
+    if result.returncode == 0:
+        logging.info(f"Copied {folder} to {destination} successfully")
     else:
-        scp_command = ["scp", "-vr", os.path.expanduser(folder), destination]
-        subprocess.run(scp_command)
-    logging.info(f"Copied {folder} to {destination}")
+        logging.error(f"Failed to copy {folder} to {destination}")
 
-def ask_user(prompt, highlight):
-    response = input(f"{prompt.replace(highlight, YELLOW + BOLD + highlight + ENDC)} ({GREEN}y{ENDC}/{RED}n{ENDC}, default {GREEN}y{ENDC}): ").strip().lower()
-    if response == '' or response == 'y':
-        return True
-    return False
+# Function to ask user for input with highlighted default value
+def ask_user(prompt, highlight, default='y'):
+    response = input(f"{prompt.replace(highlight, YELLOW + BOLD + highlight + ENDC)} ({GREEN}y{ENDC}/{RED}n{ENDC}, default {GREEN}{default}{ENDC}): ").strip().lower()
+    if response == '':
+        return default == 'y'
+    return response == 'y'
 
+# Function to ask user for transfer method
 def ask_transfer_method():
+    os.system('cls' if os.name == 'nt' else 'clear')
     print(f"{YELLOW}{BOLD}Select transfer method:{ENDC}")
     print(f"{GREEN}1. rsync{ENDC} (Recommended for faster, incremental transfer, and resumes from interruptions)")
-    print(f"{RED}2. scp{ENDC} (Simple, secure copy, but restarts the transfer from the beginning if interrupted)")
     while True:
         choice = input("Enter the number of your choice (default 1): ").strip()
         if choice == '' or choice == '1':
             return 'rsync'
-        elif choice == '2':
-            return 'scp'
         else:
-            print(f"{RED}Invalid choice. Please enter 1 or 2.{ENDC}")
+            print(f"{RED}Invalid choice. Please enter 1.{ENDC}")
 
+# Function to find non-hidden directories in home folder
 def find_non_hidden_directories():
     home_dir = os.path.expanduser("~")
     directories = [d for d in os.listdir(home_dir) if os.path.isdir(os.path.join(home_dir, d)) and not d.startswith('.')]
     return directories
 
+# Function to check if Xcode is installed
 def check_for_xcode():
     return os.path.exists("/Applications/Xcode.app")
 
+# Function to center text within a fixed width
+def center_within_width(text, width):
+    text_width = len(text)
+    padding = (width - text_width) // 2
+    return ' ' * padding + text
+
 if __name__ == "__main__":
+    # Section: Check Applications
+    logging.info("*** STARTING A NEW RUN OF THE SCRIPT ****")
     results = check_applications_folder()
     save_results_to_file(results)
     save_non_intel_apps_to_file(results)
     save_universal_apps_to_file(results)
     logging.info("Results saved to 'universal_binaries_report.txt', 'non_intel_apps.txt', and 'universal_apps.txt'")
-    
+
+    # Section: Get User Input for IP and Username
+    os.system('cls' if os.name == 'nt' else 'clear')
     remote_ip = input(f"Please enter the {YELLOW}{BOLD}IP address{ENDC} of the older Mac: ")
     username = input(f"Please enter the {YELLOW}{BOLD}username{ENDC} of the older Mac: ")
+    print("")
 
-    transfer_method = ask_transfer_method()
-
+    # Section: Recommended to Copy
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"\n{DARK_MAGENTA}{'='*50}{ENDC}\n\n{BOLD}{MAGENTA}{center_within_width('Recommended to Copy (default is Yes)', 50)}{ENDC}\n\n{DARK_MAGENTA}{'='*50}{ENDC}\n")
     copy_apps = ask_user("Do you want to copy applications?", "applications")
+    print("")
     copy_xcode = False
     if check_for_xcode():
         copy_xcode = ask_user("Xcode is a large application. Do you want to copy Xcode?", "Xcode")
-    copy_library = ask_user("Do you want to copy the entire Library directory?", "Library")
+        print("")
+    copy_preferences = ask_user("Do you want to copy preferences?", "preferences")
+    print("")
+    copy_documents = ask_user("Do you want to copy documents?", "documents")
+    print("")
+    copy_mail = ask_user("Do you want to copy mail data?", "mail data")
+    print("")
+    copy_calendars = ask_user("Do you want to copy calendar data?", "calendar data")
+    print("")
+    copy_desktop = ask_user("Do you want to copy the Desktop directory?", "Desktop")
+    print("")
+    copy_pictures = ask_user("Do you want to copy the Pictures directory?", "Pictures")
+    print("")
+    copy_music = ask_user("Do you want to copy the Music directory?", "Music")
+    print("")
+    copy_movies = ask_user("Do you want to copy the Movies directory?", "Movies")
+    print("")
+    copy_public = ask_user("Do you want to copy the Public directory?", "Public")
+    print("")
+    copy_fonts = ask_user("Do you want to copy the Fonts directory?", "Fonts")
+    print("")
+    copy_dotfiles = ask_user("Do you want to copy dotfiles (like .bash_profile, .zshrc, .gitconfig)?", "dotfiles")
+    print(f"{DARK_MAGENTA}{'='*50}{ENDC}\n")
 
-    copy_preferences = copy_documents = copy_mail = copy_calendars = False
-    if not copy_library:
-        copy_preferences = ask_user("Do you want to copy preferences?", "preferences")
-        copy_documents = ask_user("Do you want to copy documents?", "documents")
-        copy_mail = ask_user("Do you want to copy mail data?", "mail data")
-        copy_calendars = ask_user("Do you want to copy calendar data?", "calendar data")
+    # Section: Optional to Copy
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"\n{DARK_MAGENTA}{'='*50}{ENDC}\n\n{BOLD}{MAGENTA}{center_within_width('Optional to Copy (default is No)', 50)}{ENDC}\n\n{DARK_MAGENTA}{'='*50}{ENDC}\n")
+    copy_library = ask_user("Do you want to copy the entire Library directory?", "Library", default='n')
+    print("")
+    copy_downloads = ask_user("Do you want to copy the Downloads directory?", "Downloads", default='n')
+    print("")
 
     non_hidden_dirs = find_non_hidden_directories()
-    copy_dirs = {directory: ask_user(f"Do you want to copy the {directory} directory?", directory) for directory in non_hidden_dirs if directory != "Library"}
+    copy_dirs = {}
+    for directory in non_hidden_dirs:
+        if directory not in ["Library", "Desktop", "Pictures", "Music", "Movies", "Public", "Documents", "Downloads"]:
+            copy_dirs[directory] = ask_user(f"Do you want to copy the {directory} directory?", directory, default='n')
+            print("")
+    print(f"{DARK_MAGENTA}{'='*50}{ENDC}\n")
 
+    # Section: Copy Selected Items
+    os.system('cls' if os.name == 'nt' else 'clear')
     if copy_apps:
-        copy_all_except_non_intel_apps(remote_ip, username, copy_xcode, transfer_method)
+        copy_all_except_non_intel_apps(remote_ip, username, copy_xcode)
         logging.info("All applications except non-intel applications have been copied")
 
     if copy_library:
-        copy_additional_folder(remote_ip, username, "~/Library", transfer_method)
+        copy_additional_folder(remote_ip, username, "~/Library")
         logging.info("Library directory has been copied")
-    else:
-        if copy_preferences:
-            copy_additional_folder(remote_ip, username, "~/Library/Preferences", transfer_method)
-            logging.info("Preferences have been copied")
-            
+
+    if copy_preferences:
+        copy_additional_folder(remote_ip, username, "~/Library/Preferences")
+        logging.info("Preferences have been copied")
+
     if copy_documents:
-        copy_additional_folder(remote_ip, username, "~/Documents", transfer_method)
+        copy_additional_folder(remote_ip, username, "~/Documents")
         logging.info("Documents have been copied")
-                
+
     if copy_mail:
-        copy_additional_folder(remote_ip, username, "~/Library/Mail", transfer_method)
+        copy_additional_folder(remote_ip, username, "~/Library/Mail")
         logging.info("Mail data has been copied")
-                
+
     if copy_calendars:
-        copy_additional_folder(remote_ip, username, "~/Library/Calendars", transfer_method)
+        copy_additional_folder(remote_ip, username, "~/Library/Calendars")
         logging.info("Calendar data has been copied")
+    
+    if copy_desktop:
+        copy_additional_folder(remote_ip, username, "~/Desktop")
+        logging.info("Desktop directory has been copied")
 
-for directory, should_copy in copy_dirs.items():
-    if should_copy:
-        copy_additional_folder(remote_ip, username, f"~/{directory}", transfer_method)
-        logging.info(f"{directory} directory has been copied")
+    if copy_downloads:
+        copy_additional_folder(remote_ip, username, "~/Downloads")
+        logging.info("Downloads directory has been copied")
 
-logging.info("The list of apps that were not copied has been saved to 'apps_to_manually_install.txt' and copied to the remote Applications folder")
+    if copy_pictures:
+        copy_additional_folder(remote_ip, username, "~/Pictures")
+        logging.info("Pictures directory has been copied")
+
+    if copy_music:
+        copy_additional_folder(remote_ip, username, "~/Music")
+        logging.info("Music directory has been copied")
+
+    if copy_movies:
+        copy_additional_folder(remote_ip, username, "~/Movies")
+        logging.info("Movies directory has been copied")
+
+    if copy_public:
+        copy_additional_folder(remote_ip, username, "~/Public")
+        logging.info("Public directory has been copied")
+
+    if copy_fonts:
+        copy_additional_folder(remote_ip, username, "~/Library/Fonts")
+        logging.info("Fonts directory has been copied")
+
+    if copy_dotfiles:
+        dotfiles = ['.bash_profile', '.zshrc', '.gitconfig']
+        for dotfile in dotfiles:
+            if os.path.exists(os.path.expanduser(f"~/{dotfile}")):
+                copy_additional_folder(remote_ip, username, f"~/{dotfile}")
+                logging.info(f"{dotfile} has been copied")
+
+    for directory, should_copy in copy_dirs.items():
+        if should_copy:
+            copy_additional_folder(remote_ip, username, f"~/{directory}")
+            logging.info(f"{directory} directory has been copied")
+
+    logging.info("The list of apps that were not copied has been saved to 'apps_to_manually_install.txt' and copied to the remote Applications folder")
